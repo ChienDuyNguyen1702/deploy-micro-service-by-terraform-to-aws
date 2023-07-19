78f69b5914829f4e0aada07cd4a93747938a89e7
chiennd172002 <96042873+ChienDuyNguyen1702@users.noreply.github.com>
2023-07-19 09:12:32 +0700

    Update userdata.sh

--- instanceAWS/userdata.sh
+++ instanceAWS/userdata.sh
@@ -41,6 +41,6 @@ git clone https://ChienDuyNguyen1702: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@gi
 # npm install
 # npm start &
 
-cd /home/ubuntu/KTPM_BMD_VDH
+cd  /home/ubuntu/deploy-micro-service-by-terraform-to-aws
 docker compose build &
 docker compose up &

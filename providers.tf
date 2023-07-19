2c8c47c630280a48f3a63e270447f0e89bf39aca
chiennd172002 <96042873+ChienDuyNguyen1702@users.noreply.github.com>
2023-07-19 07:33:00 +0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "github-oidc-terraform-aws-tfstates-chiennd1702"
-    key    = "infra.tfstate"
-    region = "ap-southeast-1"
+    bucket = ${{ secrets.AWS_BUCKET_NAME }}
+    key    = ${{ secrets.AWS_BUCKET_KEY_NAME }}
+    region = ${{ secrets.AWS_REGION }}
   }
 }
